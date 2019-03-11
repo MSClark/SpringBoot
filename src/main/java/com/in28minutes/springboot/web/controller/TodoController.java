206e28550d86cb76277eee76412df05fa2c10d66
msclark <msclark3@asu.edu>
2019-03-11 16:57:15 -0700

    added name attr to session

--- src/main/java/com/in28minutes/springboot/web/controller/TodoController.java
+++ src/main/java/com/in28minutes/springboot/web/controller/TodoController.java
@@ -7,11 +7,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.in28minutes.springboot.web.service.LoginService;
 import com.in28minutes.springboot.web.service.TodoService;
 
 @Controller
+@SessionAttributes("name")
 public class TodoController {
 	
 	@Autowired
@@ -19,7 +21,8 @@ public class TodoController {
 	
 	@RequestMapping(value="/list-todos", method = RequestMethod.GET)
 	public String showTodosList(ModelMap model){
-		model.put("todos", service.retrieveTodos("matthew"));
+		String name = (String) model.get("name");
+		model.put("todos", service.retrieveTodos(name));
 		return "list-todos";
 	}
 	
